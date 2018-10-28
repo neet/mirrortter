bbb959b2cd8e8e265597d34d6141ad4a2ee3246a
neet <n33t5hin@gmail.com>
2018-10-29 02:11:21 +0900

    Use spapshot

--- src/utils/__test__/roundContentWithLimit.spec.ts
+++ src/utils/__test__/roundContentWithLimit.spec.ts
@@ -5,19 +5,12 @@ describe('roundContentWithLimit', () => {
   it ('Rounds text when the content is bigger than the limit', () => {
     const result = roundContentWithLimit('1234567890'.repeat(30), ['additional'], 240);
 
-    expect(result).toBe(
-      '12345678901234567890123456789012345678901234567890' +
-      '12345678901234567890123456789012345678901234567890' +
-      '12345678901234567890123456789012345678901234567890' +
-      '12345678901234567890123456789012345678901234567890' +
-      '1234567890123456789012345678901234567890' +
-      'additional ...',
-    );
+    expect(result).toMatchSnapshot();
   });
 
   it ('Won\'t round text in the content is less than or equal to the limit', () => {
     const result = roundContentWithLimit('1234567890', ['additional'], 240);
 
-    expect(result).toBe('1234567890 additional');
+    expect(result).toMatchSnapshot();
   });
 });
